2d3268a4e03f87d800b6a87c5d03dc5e9022e323
Cowan-Wilce <rosie.cowan-wilce@accenture.com>
2021-12-14 11:52:03 +0000

    Changed to imput from an ArrayList

--- src/controllers/StockController.java
+++ src/controllers/StockController.java
@@ -1,50 +1,40 @@
 package controllers;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StockController {
 
-    public static void main(String[] args) {
-        StockController sc = new StockController();
-        sc.printMenu();
-    }
-
+    private static
     Scanner scn = new Scanner(System.in);
 
-    private void printMenu() {
-        System.out.println("Which manufacturer would you like to view our stock range for?");
-        System.out.println("1 - Apple");
-        System.out.println("2 - Samsung");
-        System.out.println("3 - Oppo");
-        System.out.println();
-
-        int input = Integer.parseInt(scn.nextLine());
-
-        switch (input) {
-            case 1:
-                System.out.println("Our current stock range for Apple is:");
-                System.out.println("iPhone SE, iPhone 12, iPhone 13");
-                System.out.println();
-                printMenu();
-
-            case 2:
-                System.out.println("Our current stock range for Samsung is:");
-                System.out.println("Samsung J3, Samsung A52, Samsung S21");
-                System.out.println();
-                printMenu();
-
-            case 3:
-                System.out.println("Our current stock range for Oppo is:");
-                System.out.println("A54, X3 Lite, X3 Pro");
-                System.out.println();
-                printMenu();
-
-
-        }
-
-
+    public static void main(String[] args) {
+        StockController m = new StockController();
+        m.printMenu();
     }
 
 
+        private void printMenu() {
+
+            ArrayList stockList = new ArrayList();
+            stockList.add("iPhone 13");
+            stockList.add("iPhone SE");
+            stockList.add("Samsung S21");
+            stockList.add("Samsung A52");
+            stockList.add("Oppo X3 Neo");
+            stockList.add("Oppo A54");
+
+            System.out.println("What would you like to do?");
+            System.out.println("1 - View all stock");
+            int input = Integer.parseInt(scn.nextLine());
+
+            switch (input) {
+                case 1:
+                    System.out.println("Our current stock range is:" + stockList);
+                    printMenu();
+            }
+
+
+    }
 }
