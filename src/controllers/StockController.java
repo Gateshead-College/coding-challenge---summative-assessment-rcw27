cb199df60d620ae1a98af49597b64ee04fd859f2
Cowan-Wilce <rosie.cowan-wilce@accenture.com>
2021-12-15 14:38:54 +0000

    Menu Options recoded, spacing and colour added for improved readability
    Closes #1

--- src/controllers/StockController.java
+++ src/controllers/StockController.java
@@ -7,6 +7,15 @@ import java.util.Scanner;
 
 public class StockController {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+
     Scanner scn = new Scanner(System.in);
     ArrayList<Stock> phones = new ArrayList();
 
@@ -32,13 +41,14 @@ public class StockController {
         System.out.println("3 - Amend current stock");
         System.out.println("4 - Add a new stock option");
         System.out.println("5 - Remove a stock option");
+        System.out.println(ANSI_RED + "6 - Exit" + ANSI_RESET);
 
         int input = Integer.parseInt(scn.nextLine());
 
         switch (input) {
             case 1:
                 for (Stock s : phones) {
-                    System.out.println("Our current stock range is:" + s.phoneName);
+                    System.out.println(ANSI_CYAN + "Our current stock range is:" + s.phoneName + ANSI_RESET);
                 }
 
                 System.out.println();
@@ -50,9 +60,17 @@ public class StockController {
             case 3:
                 printStockEditMenu();
             case 4:
-                System.out.println("This option is still under construction, please try again later.");
+                System.out.println(ANSI_RED + "This option is still under construction, please try again later." + ANSI_RESET);
+                System.out.println();
+                System.out.println();
+                printMenu();
             case 5:
-                System.out.println("This option is still under construction, please try again later.");
+                System.out.println(ANSI_RED + "This option is still under construction, please try again later." + ANSI_RESET);
+                System.out.println();
+                System.out.println();
+                printMenu();
+            case 6:
+                System.exit(0);
 
         }
 
@@ -75,21 +93,33 @@ public class StockController {
         if (input <= phones.size()) {
             selectedPhone = phones.get(input - 1);
         } else {
-            System.out.println("Invalid Option.Try again");
+            System.out.println(ANSI_RED + "Invalid Option.Try again" + ANSI_RESET);
             printStockInfoMenu();
         }
 
 
         System.out.println("Loading information for  " + selectedPhone.phoneName);
         System.out.println();
-        System.out.println("ID: " + selectedPhone.productID + "  Manufacturer: " + selectedPhone.manufacturer +
-                "  Price in GBP: " + selectedPhone.price + "  Quantity in stock: " + selectedPhone.quantityStocked);
+        System.out.println(ANSI_CYAN + "ID: " + selectedPhone.productID + "  Manufacturer: "
+                + selectedPhone.manufacturer + "  Price in GBP: " + selectedPhone.price +
+                "  Quantity in stock: " + selectedPhone.quantityStocked + ANSI_RESET);
+        System.out.println();
+        System.out.println();
+        System.out.println("Would you like to view another product? Y/N");
+        System.out.println("If no you will be returned to the main menu.");
+        System.out.println();
 
+        if (scn.nextLine().equalsIgnoreCase("Y")) {
+            printStockInfoMenu();
+        } else {
+            printMenu();
+        }
     }
 
     private void printStockEditMenu() {
 
         System.out.println("Which item of stock would you like to edit?");
+        System.out.println();
         int Counter = 1;
         for (Stock s : phones) {
             System.out.println(Counter + " - " + s.phoneName);
@@ -110,122 +140,168 @@ public class StockController {
         System.out.println("3 - Price ");
         System.out.println("4 - Quantity in Stock ");
         System.out.println("5 - Phone Name ");
+        System.out.println(ANSI_RED + "6 - Exit" + ANSI_RESET);
+        System.out.println();
 
         int input = Integer.parseInt(scn.nextLine());
 
         switch (input) {
             case 1:
-                System.out.println("Current ID is set as: " + phone.productID);
+                System.out.println("Current ID is set as: " + ANSI_BLUE + phone.productID + ANSI_RESET);
                 System.out.println("What would you like to change this to?");
                 int newID = Integer.parseInt(scn.nextLine());
-                System.out.println("Are you sure you want to change the ID from " + phone.productID +
-                        " to this: " + newID + " Y/N ");
+                System.out.println("Are you sure you want to change the ID from " + ANSI_BLUE + phone.productID + ANSI_RESET +
+                        " to this: " + ANSI_CYAN + newID + ANSI_RESET + " Y/N ");
                 if (scn.nextLine().equalsIgnoreCase("Y")) {
                     phone.productID = newID;
                     System.out.println("Product ID has now been updated.");
+                    System.out.println();
+                    System.out.println();
                 } else {
                     System.out.println("Changes not saved.");
+                    System.out.println();
+                    System.out.println();
                 }
                 System.out.println("Would you like to edit another product? Y/N");
                 System.out.println("If no you will be returned to the main menu.");
+                System.out.println();
 
                 if (scn.nextLine().equalsIgnoreCase("Y")) {
+                    System.out.println();
                     printStockEditMenu();
                 } else {
+                    System.out.println();
                     printMenu();
                 }
 
 
                 break;
             case 2:
-                System.out.println("Current Manufacturer is set as: " + phone.manufacturer);
+                System.out.println("Current Manufacturer is set as: " + ANSI_BLUE + phone.manufacturer + ANSI_RESET);
+                System.out.println();
                 System.out.println("What would you like to change this to?");
+                System.out.println();
                 String newManufacturer = (scn.nextLine());
-                System.out.println("Are you sure you want to change the Manufacturer from " + phone.manufacturer +
-                        " to this: " + newManufacturer + " Y/N ");
+                System.out.println("Are you sure you want to change the Manufacturer from " + ANSI_BLUE +
+                        phone.manufacturer + ANSI_RESET +" to this: "
+                        + ANSI_CYAN + newManufacturer + ANSI_RESET + " Y/N ");
+                System.out.println();
                 if (scn.nextLine().equalsIgnoreCase("Y")) {
                     phone.manufacturer = newManufacturer;
                     System.out.println("Manufacturer has now been updated.");
+                    System.out.println();
                 } else {
                     System.out.println("Changes not saved.");
+                    System.out.println();
                 }
                 System.out.println("Would you like to edit another product? Y/N");
                 System.out.println("If no you will be returned to the main menu.");
+                System.out.println();
 
                 if (scn.nextLine().equalsIgnoreCase("Y")) {
+
+                    System.out.println();
                     printStockEditMenu();
+
                 } else {
+                    System.out.println();
                     printMenu();
                 }
 
                 break;
             case 3:
-                System.out.println("Current Price is set as: " + phone.price);
+                System.out.println("Current Price is set as: " + ANSI_BLUE + phone.price + ANSI_RESET);
                 System.out.println("What would you like to change this to?");
+                System.out.println();
                 double newPrice = Double.parseDouble(scn.nextLine());
-                System.out.println("Are you sure you want to change the Price from " + phone.productID +
-                        " to this: " + newPrice + " Y/N ");
+                System.out.println("Are you sure you want to change the Price from " + ANSI_BLUE + phone.productID + ANSI_RESET +
+                        " to this: " + ANSI_CYAN + newPrice + ANSI_RESET + " Y/N ");
+                System.out.println();
                 if (scn.nextLine().equalsIgnoreCase("Y")) {
                     phone.price = newPrice;
                     System.out.println("Price has now been updated.");
+                    System.out.println();
                 } else {
                     System.out.println("Changes not saved.");
+                    System.out.println();
                 }
                 System.out.println("Would you like to edit another product? Y/N");
                 System.out.println("If no you will be returned to the main menu.");
+                System.out.println();
 
                 if (scn.nextLine().equalsIgnoreCase("Y")) {
+                    System.out.println();
                     printStockEditMenu();
                 } else {
+                    System.out.println();
                     printMenu();
+
                 }
 
                 break;
             case 4:
-                System.out.println("Current Quantity in Stock is set as: " + phone.quantityStocked);
+                System.out.println("Current Quantity in Stock is set as: " + ANSI_BLUE + phone.quantityStocked + ANSI_RESET);
                 System.out.println("What would you like to change this to?");
+                System.out.println();
                 int newQuantity = Integer.parseInt(scn.nextLine());
-                System.out.println("Are you sure you want to change the quantity in stock from " + phone.quantityStocked +
-                        " to this: " + newQuantity + " Y/N ");
+                System.out.println("Are you sure you want to change the quantity in stock from " +
+                        ANSI_BLUE + phone.quantityStocked + ANSI_RESET + " to this: "
+                        + ANSI_CYAN + newQuantity + ANSI_RESET+ "Y/N");
+                System.out.println();
                 if (scn.nextLine().equalsIgnoreCase("Y")) {
                     phone.quantityStocked = newQuantity;
                     System.out.println("Quantity in stock has now been updated.");
+                    System.out.println();
                 } else {
                     System.out.println("Changes not saved.");
+                    System.out.println();
                 }
                 System.out.println("Would you like to edit another product? Y/N");
                 System.out.println("If no you will be returned to the main menu.");
+                System.out.println();
 
                 if (scn.nextLine().equalsIgnoreCase("Y")) {
+                    System.out.println();
                     printStockEditMenu();
                 } else {
+                    System.out.println();
                     printMenu();
                 }
 
                 break;
             case 5:
-                System.out.println("Current Phone Name is set as: " + phone.phoneName);
+                System.out.println("Current Phone Name is set as: " + ANSI_BLUE + phone.phoneName + ANSI_RESET);
                 System.out.println("What would you like to change this to?");
+                System.out.println();
                 String newName = (scn.nextLine());
-                System.out.println("Are you sure you want to change the phone name from " + phone.phoneName +
-                        " to this: " + newName + " Y/N ");
+                System.out.println("Are you sure you want to change the phone name from " + ANSI_BLUE + phone.phoneName + ANSI_RESET +
+                        " to this: " + ANSI_CYAN +  newName + ANSI_RESET + " Y/N ");
+                System.out.println();
                 if (scn.nextLine().equalsIgnoreCase("Y")) {
                     phone.phoneName = newName;
                     System.out.println("Phone Name has now been updated.");
+                    System.out.println();
                 } else {
                     System.out.println("Changes not saved.");
+                    System.out.println();
                 }
                 System.out.println("Would you like to edit another product? Y/N");
                 System.out.println("If no you will be returned to the main menu.");
+                System.out.println();
 
                 if (scn.nextLine().equalsIgnoreCase("Y")) {
+                    System.out.println();
                     printStockEditMenu();
                 } else {
+                    System.out.println();
                     printMenu();
                 }
                 break;
+            case 6:
+                System.exit(1);
             default:
-                System.out.println("Invalid option. Please try again");
+                System.out.println();
+                System.out.println(ANSI_RED + "Invalid option. Please try again" + ANSI_RESET);
                 printStockEditMenu();
 
         }
